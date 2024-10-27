9e7071ffa412ea807e6edd68c1e71d145515ea49
salah <salahswidan212@gmail.com>
2024-10-27 19:27:14 +0300

    creatge home repo method and fairure file

--- lib/Features/home/data/repos/home_repo.dart
+++ lib/Features/home/data/repos/home_repo.dart
@@ -1,5 +1,9 @@
+import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/core/utlis/errors/failures.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo {
- fetchBestSellerBooks();
-    fetchFeaturedBooks();
+ Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
+ Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
 
 }
